bd2893a701bfd026ed7cfd745a1bf67b1c8e0356
pro-masud <promasudbd@gmail.com>
2025-01-08 09:48:50 +0600

    feat: team member section item responsive apply javascript

--- exdos-html/assets/js/exdos-main.js
+++ exdos-html/assets/js/exdos-main.js
@@ -70,6 +70,20 @@
               nextEl: ".swiper-team-button-prev",
               prevEl: ".swiper-team-button-next",
             },
+            breakpoints: {
+              0: {
+                slidesPerView: 1,
+              },
+              768: {
+                slidesPerView: 2,
+              },
+              992: {
+                slidesPerView: 3,
+              },
+              1200: {
+                slidesPerView: 4,
+              },
+            },
         });
 
     });
